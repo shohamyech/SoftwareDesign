59100cfb3eae4a80919b9a02ecd510ec72d2c307
אביחי סוריה <soria@g.jct.ac.il>
2022-02-20 19:56:05 +0200

    SO TRUE BROTHER

---
+++ src/ex01/B.java
@@ -0,0 +1,21 @@
+package ex01;
+
+public class B implements C {
+
+    int num;
+
+    public B(int num) {
+        this.num = num;
+        System.out.println("B created, num " + num);
+    }
+
+    @Override
+    public void f1() {
+        System.out.println("f1");
+    }
+
+    @Override
+    public void f2() {
+        System.out.println("f2");
+    }
+}
---
+++ src/ex01/C.java
@@ -0,0 +1,6 @@
+package ex01;
+
+public interface C {
+    void f1();
+    void f2();
+}

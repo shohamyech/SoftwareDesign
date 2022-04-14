1783aedcccb78500cf1d210dbdb0cffc0866007c
shohamyech <shohamyech@gmail.com>
2022-04-14 16:36:22 +0300

    initial 05 commit

---
+++ ex05/src/DL/Customer.java
@@ -0,0 +1,51 @@
+package DL;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+
+public class Customer {
+    private long id;
+    private String name;
+    private int tier;
+
+    public Customer(String customerInfo)
+    {
+         //To Do
+    }
+
+    public Customer(long Cid,String Cname, int Ctier)
+    {
+        setId(Cid);
+        setName(Cname);
+        setTier(Ctier);
+    }
+
+    public String toString()
+    {
+        return "customer: "+ getId() + " name: "+ getName() +" tier: "+ getTier()+"\n";
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+}

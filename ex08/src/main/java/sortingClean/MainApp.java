22b19c7b94069dc2ce28a382f1f934015b1edfa7
shohamyech <shohamyech@gmail.com>
2022-05-25 21:53:13 +0300

    Thanks for the fishy

--- ex08/src/main/java/sortingClean/MainApp.java
+++ ex08/src/main/java/sortingClean/MainApp.java
@@ -1,11 +1,13 @@
 package sortingClean;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // TODO: Add java classes (in separate files for annotations and aspects)
 public class MainApp {
     public static void main(String[] args) {
         // TODO: Change this line to initialize an injected AlgorithmRunner
         AlgorithmRunner algorithmRunner = new AlgorithmRunner();
-
         algorithmRunner.runAlgorithms();
     }
     // TODO: Add producers
--- ex08/src/main/java/sortingClean/Measurer.java
+++ ex08/src/main/java/sortingClean/Measurer.java
@@ -1,19 +1,63 @@
 package sortingClean;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Aspect
 public class Measurer {
+
+    long total_time = 0, bubble_time = 0, insert_time = 0, quick_time = 0, merge_time = 0;
+    long total_count = 0, bubble_count = 0, insert_count = 0, quick_count = 0, merge_count = 0;
+    long current_time;
+
     @Pointcut("@annotation(Measurable) && execution(* *(..))")
     private void selectMeasurable(){}
 
-    @Before("selectMeasurable()")
-    public void beforeMeasurable(JoinPoint jp){
-        System.out.println("Calling loggable function " + jp.getSignature().getName() + " from "+jp.getThis() +"\n");
+    @Pointcut("execution(void sortingClean.AlgorithmRunner.runAlgorithms(..))")
+    private void selectRunner(){}
+
+    @After("selectRunner()")
+    public void afterRunner(JoinPoint jp)
+    {
+        System.out.println("Total time of running all sort functions was "+total_time+" ms");
+        System.out.println("In detail:");
+        if (bubble_time != 0)
+            System.out.println("Function sort in BubbleSort ran "+bubble_count+" times and took in total "+bubble_time+" ms");
+        if (quick_time != 0)
+            System.out.println("Function sort in QuickSort ran "+quick_count+" times and took in total "+quick_time+" ms");
+        if (merge_time != 0)
+            System.out.println("Function sort in MergeSort ran "+merge_count+" times and took in total "+merge_time+" ms");
+        if (insert_time != 0)
+            System.out.println("Function sort in InsertionSort ran "+insert_count+" times and took in total "+insert_time+" ms");
     }
 
+
+    @Before("selectMeasurable()")
+    public void beforeMeasurable(JoinPoint jp){
+        current_time = System.currentTimeMillis();
+    }
+
+    @After("selectMeasurable()")
+    public void afterMeasurable(JoinPoint jp){
+        current_time = System.currentTimeMillis() - current_time;
+        total_time += current_time;
+
+        String current_algo = jp.getThis().toString();
+        current_algo = current_algo.split("\\.")[1].split("@")[0];
+
+        switch (current_algo)
+        {
+            case "QuickSort": quick_time += current_time;quick_count++;break;
+            case "BubbleSort": bubble_time += current_time;bubble_count++;break;
+            case "InsertionSort": insert_time += current_time;insert_count++;break;
+            case "MergeSort": merge_time += current_time;merge_count++;break;
+        }
+    }
 
 }

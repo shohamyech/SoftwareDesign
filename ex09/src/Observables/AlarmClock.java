058e951b3387ff0cd4224a95dfa8c073f3e419d7
אביחי סוריה <soria@g.jct.ac.il>
2022-06-02 16:21:26 +0300

    FINISHED THE PROJECT

---
+++ ex09/src/Observables/AlarmClock.java
@@ -0,0 +1,30 @@
+package Observables;
+
+import WS1.Observables.AlarmClockRecord;
+import WS1.Observables.AlarmListener;
+
+import java.util.ArrayList;
+
+public class AlarmClock
+{
+    public final int CLOCK_INTERVAL_MILLIS = 100;
+    protected static AlarmClock instance = null;
+    private ArrayList<AlarmClockRecord> itsAlarmClockRecords = new ArrayList();
+
+    protected AlarmClock() {}
+    public static AlarmClock theInstance()
+    {
+        if(null==instance)
+            instance = new AlarmClock();
+        return instance;
+    }
+
+    protected void tic(){
+        //TODO: fix
+    }
+
+    public void register(int interval, AlarmListener pal) {
+        //TODO: fix
+    }
+}
+
